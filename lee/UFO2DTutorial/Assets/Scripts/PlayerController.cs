600573e464947a9439094466bb2720f46738d6f5
charismafight <charismafight@hotmail.com>
2018-04-17 00:02:53 +0800

    init of ufo2d

---
+++ lee/UFO2DTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("横向")]
+    public float x;
+    [Header("纵向")]
+    public float y;
+    [Header("推力")]
+    public float push;
+    [Header("碰撞器")]
+    Rigidbody2D rigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        Vector2 v = new Vector2(x, y);
+
+        rigidbody.AddForce(push * v);
+    }
+}
