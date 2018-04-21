53a1fdebdbdfe6acf14dfc9fd46b77d7dc2d383b
charismafight <charismafight@hotmail.com>
2018-04-22 00:28:43 +0800

    init dafeiji

---
+++ lee/Plane/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    private const float w = 6.71f;
+    private const float l = 9.15f;
+    Rigidbody2D rigidbody;
+    public float v;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(Utility.GetVector2() * v);
+    }
+}
---
+++ lee/Plane/Assets/Scripts/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility : MonoBehaviour
+{
+    public static Vector2 GetVector2()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        return new Vector2(x, y);
+    }
+}
