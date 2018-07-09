8e4ec82bbd9b825c8b03a3436e076685b3bf444d
charismafight-home <charismafight@hotmail.com>
2018-07-09 23:24:12 +0800

    add environment and character animations

---
+++ lee/Parkour/Assets/Scripts/HeroMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMove : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        GameObject.Find("hero").transform.Translate(0.5f, 0, 0);
+    }
+}
