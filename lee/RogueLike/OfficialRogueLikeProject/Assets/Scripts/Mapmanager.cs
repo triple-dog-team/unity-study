d8902b01729dff09bd2c7c53de9d6ebd74ccd7a4
Lee <charismafight@hotmail.com>
2019-02-19 22:12:51 +0800

    初始化地板和外墙

---
+++ lee/RogueLike/OfficialRogueLikeProject/Assets/Scripts/Mapmanager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mapmanager : MonoBehaviour
+{
+    //定义unity编辑器用属性集合，可以把prefab直接拖到里面作为随机池
+    public GameObject[] outWall;
+    public List<GameObject> floor;
+
+    public int Rows = 10;
+    public int Columns = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //地图初始化
+    void InitMap()
+    {
+        //遍历行x列个所有的格子
+        //围墙算法：如果0列或0行，则肯定是围墙（左边和下边），同时如果最大行或者最大列也是围墙（上边和右边）
+        for (int x = 0; x < Rows; x++)
+        {
+            for (int y = 0; y < Columns; y++)
+            {
+                if (x == 0 || y == 0 || x == Columns - 1 || y == Rows - 1)
+                {
+                    var r = Random.Range(0, outWall.Length);
+                    //构造围墙
+                    GameObject.Instantiate(outWall[r], new Vector3(x, y, 0), Quaternion.identity);
+                }
+                else
+                {
+                    var r = Random.Range(0, floor.Count);
+                    //构造围墙
+                    GameObject.Instantiate(floor[r], new Vector3(x, y, 0), Quaternion.identity);
+
+                }
+            }
+        }
+    }
+}
--- lee/RogueLike/README.md
+++ lee/RogueLike/README.md
@@ -15,3 +15,23 @@
 2. 同一物体的动画不要直接拖到scene中，生成物体后往物体里面拖也可以生成动画相关anmi文件
 3. Controller（状态机）如果功能相似或者重复应该通过创建override（子类）来实现，重写的内容要定义在被重写的controller里面，共享Controller逻辑（但其实还是多出一个文件），绑定不同状态变化后的动画
 4. prefab创建好之后scence中的对应文件可以删除掉，以后仅用prefab即可
+5. 在Scence中创建了GameObject之后，无论它是否有图片或者效果，它的脚本都会被执行，所以这里创建了一个空的对象并把空对象作为了初始化地图的管理器。
+6. 脚本中定义public的GameObject属性可以直接装载从prefab中拖入的控件，然后直接被初始化或者update调用，这点很好用
+7. 定义InitMap()方法给start调用，并在该方法中用简单算法判断调用GameObject.Instantiate的实例化游戏对象方法，随机从集合中构造出围墙对象，坐标根据循环中的行列即可，但这里默认会拼接上图片的大小，这点不知道怎么做到的；同时GameObject.Instantiate第三个参数可以提供对游戏对象的旋转Quaternion.identity（不旋转）。
+8. unity中，坐标的1单位是1米，如果我们使用默认用图片构造出的prefab的话它的scale也是1，1，1，所以按如下代码可以正确的生成出大小为1的一圈墙壁
+
+```
+
+        for (int x = 0; x < Rows; x++)
+        {
+            for (int y = 0; y < Columns; y++)
+            {
+                if (x == 0 || y == 0 || x == Columns - 1 || y == Rows - 1)
+                {
+                    var r = Random.Range(0, outWall.Length);
+                    //构造围墙
+                    GameObject.Instantiate(outWall[r], new Vector3(x, y, 0), Quaternion.identity);
+                }
+            }
+        }
+```
